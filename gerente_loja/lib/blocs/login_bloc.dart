c10f9040cc5d6d0b33edcdebe11ba2f8750cd061
Lucas Sales <lucas@yupistudios.com>
2021-10-21 11:27:19 -0300

    BLoC e Validators

---
+++ gerente_loja/lib/blocs/login_bloc.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:gerente_loja/validators/login_validators.dart';
+import 'package:rxdart/rxdart.dart';
+
+class LoginBloc extends BlocBase with LoginValidators {
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
+
+  Stream<String> get outEmail =>
+      _emailController.stream.transform(validateEmail);
+  Stream<String> get outPassword =>
+      _passwordController.stream.transform(validatePassword);
+
+  @override
+  void dispose() {
+    _emailController.close();
+    _passwordController.close();
+    super.dispose();
+  }
+}
---
+++ gerente_loja/lib/validators/login_validators.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+class LoginValidators {
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains("@")) {
+      sink.add(email);
+    } else {
+      sink.addError("Insira um e-mail válido");
+    }
+  });
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 4) {
+      sink.add(password);
+    } else {
+      sink.addError("Senha inválida, deve conter pelo menos 4 caracteres");
+    }
+  });
+}
